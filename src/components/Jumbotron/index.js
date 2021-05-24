dcde32f371be0e6b1bf6268f4752212bbd322498
Nate J <jamis.walsh@gmail.com>
2021-05-23 17:59:19 -0700

    addt. styling

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Jumbotron() {
     return (
-        <div class="jumbotron jumbotron-fluid text-center">
+        <div class="jumbotron jumbotron-fluid text-center bg-dark text-white">
             <div class="container">
                 <h1 class="display-4">Employee Directory</h1>
-                <p class="lead">Sort employees alphabetically by clicking the first or last name column below, or find a specific employee by using the search bar.</p>
+                <p class="lead pb-3">Sort employees alphabetically by clicking the first or last name column below, or find a specific employee by using the search bar.</p>
             </div>
         </div>
     )
